eafabd0f61e3460ddde2129b6fc914d43163f843
Olena Boldysheva <adv55577@gmail.com>
2022-04-15 22:08:54 +0300

    update module-3

---
+++ pathlib_md.py
@@ -0,0 +1,26 @@
+# from pathlib import Path
+import sys
+
+# p=Path('/Users/grejule/Documents/GitHub-home/Python/Python-practice')
+# p_2=Path('/Users/grejule/Documents/GitHub-home/Python/Python-practice/module_4.py')
+
+# print(p.is_dir())
+# print(p_2.is_file())
+# print(p_2.exists())
+
+# print(p)
+# print(p.parent)
+# print(p.name)
+# print(p.suffix)
+
+
+# for i in p.iterdir():
+#     print(i.name)
+
+for arg in sys.argv:
+    print(arg)
+
+# def main():
+#     print(sys.argv)
+
+# main()
