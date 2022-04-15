82cc93f6eaec58369b4051b9fd917184aa5d747a
Olena Boldysheva <adv55577@gmail.com>
2022-04-15 15:51:15 +0300

    module_4

---
+++ module_4.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20]
+
+# odd_numbers = numbers[::2]
+# even_numbers = numbers[1::2]
+# three_numbers = numbers[2:10:3]
+
+numbers_copy = numbers[:]
+numbers_copy_2 = numbers.copy()
+numbers_copy_3 = list(numbers)
+
+
+# print(numbers)
+
+# print(numbers_copy)
+# print(numbers_copy_2)
+# print(numbers_copy_3)
+
+
